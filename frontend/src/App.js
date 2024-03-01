32f53e3c130762e3386238b0a4a24bdd6669c2fc
qkr2713 <qkr2713@naver.com>
2024-03-01 21:00:56 +0900

    [FE]gpt

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import EnglishStudyPage from "./pages/main/MainPage";
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import StudyPage from "./pages/main/StudyPage"
+
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path='/' element={<EnglishStudyPage />} />
+        <Route path='/study' element={<StudyPage />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ frontend/src/pages/ChatPage.js
@@ -0,0 +1,14 @@
+import Reract, {useEffect} from 'react';
+import axios from "axios";
+import axiosInstance from "@utils/axiosInstance";
+import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+
+function ChatPage() {
+  const navigate = useNavigate();
+  
+
+  
+
+}
