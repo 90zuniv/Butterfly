cba4cc04eb84491129a58c3ca5becb7052b67330
yoongj <skdbswjd16@gmail.com>
2024-03-14 17:35:19 +0900

    to reduce token

--- AI/same.py
+++ AI/same.py
@@ -1,12 +1,24 @@
-a= 'text.txt'
-b= 'text_auto.txt'
+# a= 'text.txt'
+# b= 'text_auto.txt'
 
-with open(a, 'r') as text:
-    a= text.readlines()
-with open(b, 'r') as text:
-    b= text.readlines()
+# with open(a, 'r') as text:
+#     a= text.readlines()
+# with open(b, 'r') as text:
+#     b= text.readlines()
 
-if a==b:
-    print('yes')
-else:
-    print('no')
+# if a==b:
+#     print('yes')
+# else:
+#     print('no')
+
+
+
+dic= {
+    'a':'aaaaa',
+    'b':'bbbbb',
+    'c':'ccccc',
+    'd':'ddddd',
+    'e':'eeeee',
+
+    }
+print(dic.keys())
